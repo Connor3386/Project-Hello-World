c02227e72145a9e9253b7f732ae0f1b21d21a8c6
Connor3386 <connorcox3386@gmail.com>
2026-04-01 09:28:29 -0700

    Dislike Button

--- new_react_project/src/Card.jsx
+++ new_react_project/src/Card.jsx
@@ -1,34 +1,34 @@
 import { useState } from "react";
 function Card({ name, title, blurb, github, skills, image }) { const [showMore, setShowMore] = useState(false);
-return (
-<div className="bg-white rounded-2xl shadow-md p-6 w-98 text-center hover: shadow-xl
-transition-all duration-200">
-<h3 className="text-xl font-bold text-gray-800">
-{name}
-</h3>
-<div className="mask-radial-at-center mask-radial-from-100%"><img className="rounded-full" src={image}/></div>
-<p className="text-blue-500 font-semibold">{title}</p> <p className="text-gray-500 mt-2">{blurb}</p>
-<button
-onClick={() => setShowMore (!showMore)} className="mt-4 text-sm text-blue-500 hover: text-blue-700 font-semibold"
->
-{showMore?"▲ Show Less":"▼ Show More"} </button>
-{showMore && (
-<div className="mt-4 pt-4 border-t border-gray-200 text-left">
-{github && (
-<p className="text-sm text-gray-600"> <a href={github}
-className="text-blue-500
-hover:underline">
-GitHub Repo</a>
-</p>
-)}
-{skills && (
-<p className="text-sm text-gray-600 mt-2">
-• Skills: {skills}!
-</p>
-)}
-</div>
-)}
-</div>
-);
+    return (
+        <div className="bg-white rounded-2xl shadow-md p-6 w-98 text-center hover: shadow-xl
+            transition-all duration-200">
+            <h3 className="text-xl font-bold text-gray-800">
+                {name}
+            </h3>
+            <div className="mask-radial-at-center mask-radial-from-100%"><img className="rounded-full" src={image}/></div>
+            <p className="text-blue-500 font-semibold">{title}</p> <p className="text-gray-500 mt-2">{blurb}</p>
+            <button
+                onClick={() => setShowMore (!showMore)} className="mt-4 text-sm text-blue-500 hover: text-blue-700 font-semibold"
+            >
+            {showMore?"▲ Show Less":"▼ Show More"} </button>
+            {showMore && (
+            <div className="mt-4 pt-4 border-t border-gray-200 text-left">
+                {github && (
+                <p className="text-sm text-gray-600"> <a href={github}
+                    className="text-blue-500
+                    hover:underline">
+                    GitHub Repo</a>
+                </p>
+                )}
+                {skills && (
+                <p className="text-sm text-gray-600 mt-2">
+                • Skills: {skills}!
+                </p>
+                )}
+            </div>
+            )}
+        </div>
+    );
 }
 export default Card;
